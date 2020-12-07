da962d5aa79b8ad344bf5718e702353125227b3b
JJangl <71086028+JJangl@users.noreply.github.com>
2020-12-07 23:23:52 +0300

    добавили новую активность

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+    }//
     @Override
     public void onStart(){
         super.onStart();
